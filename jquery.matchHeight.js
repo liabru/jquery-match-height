64b9a547f463a0813f78150bb2e81b256d05bc10
liabru <liabru@brm.io>
2015-01-17 12:39:50 +0000

    fix for single item rows, closes #48

--- jquery.matchHeight.js
+++ jquery.matchHeight.js
@@ -199,8 +199,10 @@
                 maxHeight = 0;
 
             // skip apply to rows with only one item
-            if (opts.byRow && $row.length <= 1)
+            if (opts.byRow && $row.length <= 1) {
+                $row.css(opts.property, '');
                 return;
+            }
 
             // iterate the row and find the max height
             $row.each(function(){
